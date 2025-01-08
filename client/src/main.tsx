aac8ba459617e9128a4736abed091ec159bd58bd
You Song Hou <ysongweb3@gmail.com>
2025-01-08 01:38:00 -0500

    chore: install dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { setupBurnerManager } from "@dojoengine/create-burner"
 import './index.css'
 import App from './App.tsx'
+import { dojoConfig } from '../dojoConfig.ts'
+import { DojoContextProvider } from './DojoContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DojoContextProvider
+      burnerManager={await setupBurnerManager(dojoConfig)}
+    >
+      <App />
+    </DojoContextProvider>
+    
   </StrictMode>,
 )
