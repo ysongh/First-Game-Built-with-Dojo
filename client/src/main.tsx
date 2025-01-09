9bb72df30c70ae79efa292c193f9b5d9a9e02d11
You Song Hou <ysongweb3@gmail.com>
2025-01-08 23:51:51 -0500

    chore: initialize and bootstrap the Dojo application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,29 @@ import { createRoot } from 'react-dom/client'
 import { setupBurnerManager } from "@dojoengine/create-burner"
 import './index.css'
 import App from './App.tsx'
+import { init } from "@dojoengine/sdk";
+import { SchemaType, schema } from "./dojo/typescript/models.gen.ts";
 import { dojoConfig } from '../dojoConfig.ts'
 import { DojoContextProvider } from './DojoContext.tsx'
 import StarknetProvider from './starknet-provider.tsx'
+
+const sdk = await init<SchemaType>(
+  {
+      client: {
+          rpcUrl: dojoConfig.rpcUrl,
+          toriiUrl: dojoConfig.toriiUrl,
+          relayUrl: dojoConfig.relayUrl,
+          worldAddress: dojoConfig.manifest.world.address,
+      },
+      domain: {
+          name: "WORLD_NAME",
+          version: "1.0",
+          chainId: "KATANA",
+          revision: "1",
+      },
+  },
+  schema
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,7 +33,7 @@ createRoot(document.getElementById('root')!).render(
       burnerManager={await setupBurnerManager(dojoConfig)}
     >
       <StarknetProvider>
-        <App />
+        <App sdk={sdk} />
       </StarknetProvider>
     </DojoContextProvider>   
   </StrictMode>,
